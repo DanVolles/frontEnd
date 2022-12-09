2cc44ac963120422739ad14923cc5456d5c4da92
Danilo <danilo.volles@outlook.com>
2022-12-08 21:40:42 -0300

    Alteração das imagens. Commit operacional.

---
+++ prj-loja-01/fake-api/img/produto.js
@@ -0,0 +1,8 @@
+[
+    {"nome":"drawing-tablet", "img":"", "valor":500},
+    {"nome":"keyboard", "img":"", "valor":1000},
+    {"nome":"laptop", "img":"", "valor":5000},
+    {"nome":"monitor", "img":"", "valor":900},
+    {"nome":"smartphone", "img":"", "valor":1500},
+    {"nome":"tablet", "img":"", "valor":1200}
+]
