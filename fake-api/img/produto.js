6f8e4442c740c764d15ec78704a746c388e4c8c6
Danilo <danilo.volles@outlook.com>
2022-12-13 18:10:53 -0300

    Produto.js Update

--- fake-api/img/produto.js
+++
@@ -1,8 +0,0 @@
-[
-    {"nome":"Tablet de Desenho","img":"https://github.com/DanVolles/frontEnd/tree/master/prj-loja-01/fake-api/img/drawing-tablet.png","valor":500},
-    {"nome":"Teclado Mecanico","img":"https://github.com/DanVolles/frontEnd/tree/master/prj-loja-01/fake-api/img/keyboard.png","valor":1000},
-    {"nome":"Laptop intel I7","img":"https://github.com/DanVolles/frontEnd/tree/master/prj-loja-01/fake-api/img/laptop.png","valor":5000},
-    {"nome":"Monitor IPS","img":"https://github.com/DanVolles/frontEnd/tree/master/prj-loja-01/fake-api/img/monitor.png","valor":900},
-    {"nome":"Celular Smartphone","img":"https://github.com/DanVolles/frontEnd/tree/master/prj-loja-01/fake-api/img/smartphone.png","valor":1500},
-    {"nome":"Tablet 9 polegadas","img":"https://github.com/DanVolles/frontEnd/tree/master/prj-loja-01/fake-api/img/tablet.png","valor":1200}
-]
---
+++ fake-api/produto.js
@@ -0,0 +1,8 @@
+[
+    {"nome":"Tablet de Desenho",   "img":"https://danvolles.github.io/frontEnd/tree/master/fake-api/img/drawing-tablet.png",  "valor":500},
+    {"nome":"Teclado Mecanico",    "img":"https://danvolles.github.io/frontEnd/tree/master/fake-api/img/keyboard.png",        "valor":1000},
+    {"nome":"Laptop intel I7",     "img":"https://danvolles.github.io/frontEnd/tree/master/fake-api/img/laptop.png",          "valor":5000},
+    {"nome":"Monitor IPS",         "img":"https://danvolles.github.io/frontEnd/tree/master/fake-api/img/monitor.png",         "valor":900},
+    {"nome":"Celular Smartphone",  "img":"https://danvolles.github.io/frontEnd/tree/master/fake-api/img/smartphone.png",      "valor":1500},
+    {"nome":"Tablet 9 polegadas",  "img":"https://danvolles.github.io/frontEnd/tree/master/fake-api/img/tablet.png",          "valor":1200}
+]
